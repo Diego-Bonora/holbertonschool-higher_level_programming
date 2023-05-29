071aa05dbc993f67592ed7ca958c3f065555c3b5
Diego-Bonora <bonoradi@gmail.com>
2023-05-29 10:30:13 -0700

    function to print all the alpabet except e and q

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 text = "{}"
 for i in range(97, 123):
-    if chr(i) != "p" and chr(i) != "e":
+    if chr(i) != "q" and chr(i) != "e":
         print(text.format(chr(i)), end="")
