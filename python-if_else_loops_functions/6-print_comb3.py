bd9f8700e2da4db96e9b4b95c1f6f799f8ff64e2
Diego-Bonora <bonoradi@gmail.com>
2023-05-30 07:03:15 -0700

    j now start at 1 insted of 0

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(0, 10):
-        if j == 0 and i == 0:
+    for j in range(1, 10):
+        if j == 1 and i == 0:
             print("{0}".format(str(i) + str(j)), end="")
         elif i != j and j > i:
             print(", {0}".format(str(i) + str(j)), end="")
