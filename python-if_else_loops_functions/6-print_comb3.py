116ad2048b146305a78aaa551448ab50db426673
Diego-Bonora <bonoradi@gmail.com>
2023-05-30 07:01:59 -0700

    function for printing all the 2 digit number combination without repetition

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if j == 0 and i == 0:
-            print(f"{str(i) + str(j)}", end="")
+            print("{0}".format(str(i) + str(j)), end="")
         elif i != j and j > i:
-            print(f", {str(i) + str(j)}", end="")
+            print(", {0}".format(str(i) + str(j)), end="")
 print("")
