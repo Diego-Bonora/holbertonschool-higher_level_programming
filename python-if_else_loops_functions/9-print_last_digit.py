37028c4668cc74ebd8357adb73ff71a7c42c6387
Diego-Bonora <bonoradi@gmail.com>
2023-05-30 10:30:53 -0700

    printing number that was told not to print

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,5 @@ def print_last_digit(number):
     last_digit = number
     if last_digit < 0:
         last_digit = last_digit * -1
+    print("{}".format(last_digit % 10), end="")
     return last_digit % 10
