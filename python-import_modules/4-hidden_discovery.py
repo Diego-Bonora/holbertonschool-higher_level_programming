1fa5779e58e0616cc04df3713f05304d77888062
Diego-Bonora <bonoradi@gmail.com>
2023-06-01 06:05:51 -0700

    fixed import

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4.pyc
-    list = dir(hidden_4.pyc)
+    import hidden_4
+    list = dir(hidden_4)
     list_no_ = []
     for i in list:
         if i[0] != '_' and i[1] != '_':
