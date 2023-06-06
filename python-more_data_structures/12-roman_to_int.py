c9e95715c06f6624ca9794daef2debb89b5c7785
Diego-Bonora <77507773+Diego-Bonora@users.noreply.github.com>
2023-06-06 19:02:11 -0300

    small fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-roman_leters = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    }
-    add = 0
-    sub = 0
-    big = 0
-    r_s = roman_string[::-1]
-    for i in range(len(r_s)):
-        if r_s[i] == 0 and r_s[i] in roman_leters:
-            add += roman_leters[r_s[i]]
-            big = roman_leters[r_s[i]]
-        elif r_s[i] in roman_leters and roman_leters[r_s[i]] < big:
-            sub += roman_leters[r_s[i]]
-        elif r_s[i] in roman_leters:
-            add += roman_leters[r_s[i]]
-            add -= sub
-            sub = 0
-            big = roman_leters[r_s[i]]
-    add -= sub
-    return add
+def roman_to_int(roman_string):
+    if roman_string and isinstance(roman_string, str):
+        roman_leters = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        add = 0
+        sub = 0
+        big = 0
+        r_s = roman_string[::-1]
+        for i in range(len(r_s)):
+            if r_s[i] == 0 and r_s[i] in roman_leters:
+                add += roman_leters[r_s[i]]
+                big = roman_leters[r_s[i]]
+            elif r_s[i] in roman_leters and roman_leters[r_s[i]] < big:
+                sub += roman_leters[r_s[i]]
+            elif r_s[i] in roman_leters:
+                add += roman_leters[r_s[i]]
+                add -= sub
+                sub = 0
+                big = roman_leters[r_s[i]]
+        add -= sub
+        return add
+    else:
+        return 0
