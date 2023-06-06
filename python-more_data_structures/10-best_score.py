bd9d6266828769c3d759b8f12c4761d36d53512b
Diego-Bonora <bonoradi@gmail.com>
2023-06-06 09:47:33 -0700

    find the biggest value from a dictionary

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        for i, x in sorted(a_dictionary.items()):
+            value = i
+    return value
