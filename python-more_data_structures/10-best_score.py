bef78fbb0253b3e812f0b5bdf094619b4b3d0f2f
Diego-Bonora <bonoradi@gmail.com>
2023-06-06 09:53:28 -0700

    fixed for finding the correct value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,9 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        for i, x in sorted(a_dictionary.items()):
+        for i in sorted(a_dictionary.values()):
             value = i
+        for key, val in a_dictionary.items():
+            if value == val:
+                return key
     return value
