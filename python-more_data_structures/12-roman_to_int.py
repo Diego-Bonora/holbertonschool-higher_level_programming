fd1dd41d6274c49f139b83fb98d34c6728f22167
Diego-Bonora <77507773+Diego-Bonora@users.noreply.github.com>
2023-06-06 18:58:14 -0300

    modified to substract when necesary

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    roman_numbers = {
+roman_leters = {
         "I": 1,
         "V": 5,
         "X": 10,
         "L": 50,
         "C": 100,
         "D": 500,
-        "M": 1000
-        }
+        "M": 1000,
+    }
     add = 0
     sub = 0
-    for i in roman_string:
-        if i == "I":
-            sub += 1
-        elif i in roman_numbers:
-            add += roman_numbers[i]
+    big = 0
+    r_s = roman_string[::-1]
+    for i in range(len(r_s)):
+        if r_s[i] == 0 and r_s[i] in roman_leters:
+            add += roman_leters[r_s[i]]
+            big = roman_leters[r_s[i]]
+        elif r_s[i] in roman_leters and roman_leters[r_s[i]] < big:
+            sub += roman_leters[r_s[i]]
+        elif r_s[i] in roman_leters:
+            add += roman_leters[r_s[i]]
             add -= sub
             sub = 0
-    add += sub
+            big = roman_leters[r_s[i]]
+    add -= sub
     return add
