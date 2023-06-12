37c61143f139b1841545060b0d1b8601fcf18b29
Diego-Bonora <bonoradi@gmail.com>
2023-06-12 06:20:43 -0700

    changed the order of the if and elif

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """ comentario """
     def __init__(self, size=0) -> None:
         if not isinstance(size, int):
-            raise ValueError("size must be >= 0")
-        elif size < 0:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
