51b2de4b837eab9e383770302b24da61fa912bf1
Diego-Bonora <bonoradi@gmail.com>
2023-06-13 09:17:24 -0700

    added documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+add_integer - function for adding 2 int numbers
+a: first input number
+b: second input number
+"""
+
+
 def add_integer(a, b=98):
+    """
+    first checks if a and b are floats or int, then returns the int add
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
