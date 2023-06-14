80e2df6a9c41b996ff2a0ede35a7d3a63a38f713
Diego-Bonora <bonoradi@gmail.com>
2023-06-14 09:50:05 -0700

    wrong error when last name is not a str

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,9 @@
 
 def say_my_name(first_name, last_name=""):
     """ comentario """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
-        raise TypeError("first_name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
