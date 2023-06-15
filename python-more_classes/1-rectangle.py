02953fdb35f66c5fcb492de0975d04eefa72dde9
Diego-Bonora <bonoradi@gmail.com>
2023-06-15 06:52:45 -0700

    small fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,8 +8,19 @@ class Rectangle:
     """ rectangle class with width and height attrivutes """
 
     def __init__(self, width=0, height=0) -> None:
-        self.__width = width
-        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
 
     @property
     def width(self):
