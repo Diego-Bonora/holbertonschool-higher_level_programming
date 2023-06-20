956a7276b6b559e75fe292f1cae8ada0886359b8
Diego-Bonora <bonoradi@gmail.com>
2023-06-20 10:50:41 -0700

    created new class that inherits from BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" comentario """
+
+
+class BaseGeometry():
+    """ comentario """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+""" comentario """
+
+
+class Rectangle(BaseGeometry):
+    """ comentario """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
