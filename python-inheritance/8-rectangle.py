3dc5939c72c99c3e69622191785390d0569f452b
Diego-Bonora <bonoradi@gmail.com>
2023-06-20 10:55:17 -0700

    small fix

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+""" comentario """
+
+
+class BaseGeometry():
+    """ comentario """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
 """ comentario """
 
 
@@ -8,6 +23,6 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         self.integer_validator("width", width)
+        self._Rectangle__width = width
         self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        self._Rectangle__height = height
