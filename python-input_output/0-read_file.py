8965774319043e70ae2bb0a00eb79b4d62775ba4
Diego-Bonora <bonoradi@gmail.com>
2023-06-23 05:37:45 -0700

    added coments

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" comentario """
+
+
 def read_file(filename=""):
+    """ comentario """
     with open(filename, encoding="utf-8") as f:
         file = f.read()
         print(file)
