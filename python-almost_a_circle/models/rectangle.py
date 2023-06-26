fd0980ea1aca3024082a3769a04522f3bbef9a92
Diego-Bonora <bonoradi@gmail.com>
2023-06-26 06:49:15 -0700

    rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ class Rectangle(Base):
     """ comentario """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         self.__widht = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
 
     @property
     def widht(self):
