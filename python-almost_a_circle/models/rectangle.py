4898dddaaf281033819ea96f469f085ee8c3ee24
Diego-Bonora <bonoradi@gmail.com>
2023-06-26 07:28:37 -0700

    __str__ function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,3 +71,6 @@ class Rectangle(Base):
             for x in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self) -> str:
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
