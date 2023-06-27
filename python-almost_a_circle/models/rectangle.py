cb23aee517dc6d93c63f7348e3f3e0ac3edff3d8
Diego-Bonora <bonoradi@gmail.com>
2023-06-27 06:57:53 -0700

    pycode

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -115,4 +115,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ Function that returns a dictionaty """
-        return {"x": self.x, "y": self.y, "id": self.id, "height": self.height, "width": self.width}
+        return {"x": self.x, "y": self.y, "id": self.id,
+                "height": self.height, "width": self.width}
