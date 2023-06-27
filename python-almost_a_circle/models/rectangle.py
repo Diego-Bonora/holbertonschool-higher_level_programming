8129d3ae5a30b1e3e1665e44822e884ac3f2201d
Diego-Bonora <bonoradi@gmail.com>
2023-06-27 06:56:37 -0700

    added a coment to the new function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -114,4 +114,5 @@ class Rectangle(Base):
                     self.y = value
 
     def to_dictionary(self):
+        """ Function that returns a dictionaty """
         return {"x": self.x, "y": self.y, "id": self.id, "height": self.height, "width": self.width}
