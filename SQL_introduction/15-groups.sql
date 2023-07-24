499b0876c1deaa2ba140c8c41579305df43e70be
Diego-Bonora <bonoradi@gmail.com>
2023-07-24 10:10:43 -0700

    ordered by number in desc order

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- lists all the scores and the number of times it apears
 
-SELECT score,COUNT(score) as number FROM second_table GROUP BY score;
+SELECT score,COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
