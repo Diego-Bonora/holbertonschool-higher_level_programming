00cdd14508e577ada62aafbafeb42bb968464d78
Diego-Bonora <bonoradi@gmail.com>
2023-07-25 07:00:42 -0700

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- create User
 
-CREATE USER if not exists "user_0d_1"@"localhost" IDENTIFIED WITH mysql_native_password BY "user_0d_1_pwd";
+CREATE USER if not exists "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+SET PASSWORD FOR "user_0d_1"@"localhost" = "user_0d_1_pwd";
 GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
 FLUSH PRIVILEGES;
