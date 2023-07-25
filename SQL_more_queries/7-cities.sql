8802501645eebaa1a63f3b2d26de000babb9d4b9
Diego-Bonora <bonoradi@gmail.com>
2023-07-25 09:42:04 -0700

    creates a table with a foreign key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create a table with a foreign key 
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256),
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
