c1c17d2b8ba3b8500f85761f1e5696ab856d69bf
Diego-Bonora <bonoradi@gmail.com>
2023-07-25 07:01:14 -0700

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- create User
 
-CREATE USER if not exists "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER if not exists "user_0d_1"@"localhost";
 SET PASSWORD FOR "user_0d_1"@"localhost" = "user_0d_1_pwd";
 GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
 FLUSH PRIVILEGES;
