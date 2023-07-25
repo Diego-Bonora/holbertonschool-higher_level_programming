d9d45a934c887cb199456980145ff0ecfa1a38e9
Diego-Bonora <bonoradi@gmail.com>
2023-07-25 10:09:56 -0700

    fix

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all cities from california
 
-SELECT * FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT * FROM cities, states WHERE cities.state_id = states.id AND states.name = "California" ORDER BY cities.id ASC;
