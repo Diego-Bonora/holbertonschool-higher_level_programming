d376625d9843673f54ae2b7c07373090e0daa5b0
Diego-Bonora <bonoradi@gmail.com>
2023-07-26 10:14:04 -0700

    lists all genres and number of shows

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all genres and number of shows
+
+SELECT tv_genres.name,COUNT(tv_show_genres.genre_id) as number_of_shows FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
