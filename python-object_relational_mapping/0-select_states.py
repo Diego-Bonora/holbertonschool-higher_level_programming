088bb6b2cc2ad651cff62a087becb7f3021d8b0b
Diego-Bonora <bonoradi@gmail.com>
2023-07-27 07:48:57 -0700

    conects to a database and selects all from the table

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost",
+                         database=sys.argv[3],
+                         user=sys.argv[1],
+                         password=sys.argv[2])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+
+    db.close()
