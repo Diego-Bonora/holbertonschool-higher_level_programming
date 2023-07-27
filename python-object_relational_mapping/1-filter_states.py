f3a53034a082ad32fd72ad2971bd9f2582b61b71
Diego-Bonora <bonoradi@gmail.com>
2023-07-27 08:55:57 -0700

    filtering by N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Conects to a database"""
+
+
+if __name__ == "__main__":
+    """ conects to a database"""
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost",
+                         database=sys.argv[3],
+                         user=sys.argv[1],
+                         password=sys.argv[2])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        if row[1][0] == "N":
+            print(row)
+
+    db.close()
