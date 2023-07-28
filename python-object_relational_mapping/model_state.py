70b02cffce826aee86b8e0ea980fd78481eaab4f
Diego-Bonora <bonoradi@gmail.com>
2023-07-28 09:54:16 -0700

    fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,10 +9,7 @@ Base = declarative_base()
 
 class State(Base):
     """ Class state """
-    __tablename__ = 'States'
-    id = Column(Integer, autoincrement="auto", unique=True,
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    def __repr__(self):
-        return "{}(name='{}')".format(self.__class__.__name__, self.name)
