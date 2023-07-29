176fcd3a802f11b5099c729d5276185204e4167d
Diego-Bonora <bonoradi@gmail.com>
2023-07-29 20:04:14 -0300

    updating a value in a database

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Inserts to a table using SQLAlchemy """
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    modified = session.query(State).filter(State.id == 2)
+    modified.name = "New Mexico"
+    session.commit()
+
+    session.close()
