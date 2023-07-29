45e4dd7183800064d25d7d2ad720d93eb5e0e337
Diego-Bonora <bonoradi@gmail.com>
2023-07-29 20:45:28 -0300

    select from 2 tables

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" lists from a table using SQLAlchemy """
+from model_state import Base, State
+from model_city import Base, City
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State.name, City.id,
+                           City.name).join(City, City.state_id == State.id).all()
+    for row in result:
+        print("{}: ({}) {}".format(row[0], row[1], row[2]))
+    session.close()
