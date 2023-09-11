a20cd7815b597d3cf0a136eb5c834ed351ad6876
Diego-Bonora <bonoradi@gmail.com>
2023-09-11 09:28:47 -0700

    change color on click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+document.querySelector("#red_header").addEventListener("click", header_color_changer);
+function header_color_changer() {
+    document.querySelector("header").style.color = "#FF0000";
+  }
