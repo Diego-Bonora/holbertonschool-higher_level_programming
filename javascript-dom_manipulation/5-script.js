76c329fb890fdcefc1137a7de7218836a5e15095
Diego-Bonora <bonoradi@gmail.com>
2023-09-12 08:46:46 -0700

    updates the header

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,4 @@
+document.querySelector("#update_header").addEventListener("click", update_header);
+function update_header() {
+    document.querySelector("header").innerHTML = `New Header!!!`;
+  }
